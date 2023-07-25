0a95a434a702d6409105cb5f6783583c05f058b7
Donna Lee <dhl287@gmail.com>
2023-07-24 21:38:03 -0700

    updated script.js

--- script.js
+++ script.js
@@ -1,51 +1,80 @@
-const apiKey = "572661e61377e7d7c006042ef76c9263";
+var apiKey = "572661e61377e7d7c006042ef76c9263";
 
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-  
-const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+var main = $('main');
+var form = $('#form');
+var search = $('#search');
+var lat = '52.5';
+var lon = '-1.95';  
+var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+var city = '';
+var searchBtn = $('#searchBtn');
 
 
-async function getWeatherByLocation(city){
-     
-         const resp = await fetch(url(city), {
-             origin: "cros" });
-         const respData = await resp.json();
-     
-           addWeatherToPage(respData);
-          
-     }
+
+function getWeatherByLocation(city) {
+    fetch(url(city), { origin: "cros" })
+      .then((resp) => resp.json())
+      .then((respData) => {
+        addWeatherToPage(respData);
+      })
+      .catch((error) => {
+        console.error("Error occurred:", error);
+      });
+  }
 
       function addWeatherToPage(data){
-          const temp = Ktoc(data.main.temp);
+          const temp = Ktof(data.main.temp);
 
           const weather = document.createElement('div')
           weather.classList.add('weather');
+          
 
           weather.innerHTML = `
-          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°F <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
           <small>${data.weather[0].main}</small>
           
           `;
 
 
-        //   cleanup 
+      
           main.innerHTML= "";
            main.appendChild(weather);
       };
 
-
-     function Ktoc(K){
-         return Math.floor(K - 273.15);
+    // Kelvin to Fahrenheit formula
+     function Ktof(K){
+         return Math.floor((K - 273.15) * 1.8 + 32);
      }
 
+     function addWeatherToPage(data){
+        const temp = Ktof(data.main.temp);
+
+        const weather = document.createElement('div')
+        weather.classList.add('weather');
+        
+
+        weather.innerHTML = `
+        <h2><img src="https://openweathermap.org/img/wn/${data.weather[1].icon}@2x.png" /> ${temp}°F <img src="https://openweathermap.org/img/wn/${data.weather[1].icon}@2x.png" /></h2>
+        <small>${data.weather[1].main}</small>
+        
+        `;
 
 
-     form.addEventListener('submit',(e) =>{
-        e.preventDefault();
+    
+        main.innerHTML= "";
+         main.appendChild(weather);
+    };
 
-        const city = search.value;
+  // Kelvin to Fahrenheit formula
+   function Ktof(K){
+       return Math.floor((K - 273.15) * 1.8 + 32);
+   }
+
+
+     form.addEventListener('submit',(event) =>{
+        event.preventDefault();
+
+        var city = search.value;
 
         if(city){
             getWeatherByLocation(city)
